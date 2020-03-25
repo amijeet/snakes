17154f086ae3373fdfc288e2b6b77f87827916e1
Amijeet Thakur <41593252+amijeet@users.noreply.github.com>
2020-03-25 21:45:32 +0530

    Add files via upload

--- snake.py
+++ snake.py
@@ -5,37 +5,96 @@ from random import randint
 
 from pygame.locals import*
 pygame.init()
-BLUE = (0,255,0)
+GREEN = (0,255,0)
 WHITE = (255,255,255)
 RED = (255,0,0)
 DISPLAY = pygame.display.set_mode((400,400))
 clock = pygame.time.Clock()
-clock.tick(30)
+FPS = 10
 snake = [[200,200], [210,200], [220,200]]
+x = 100
+y = 100
+direction = 0
 
-
-
-def draw_snake(snake):
+def draw_snake():
 	for s in snake:
-		pygame.draw.rect( DISPLAY, BLUE, [s[0], s[1], 10, 10], 0 )
+		pygame.draw.rect( DISPLAY, GREEN, [s[0], s[1], 10, 10], 0 )
+def draw_mouse(mouse_co):
+	pygame.draw.rect( DISPLAY, RED, [mouse_co[0], mouse_co[1], 10, 10], 0 )
 
-def mouse():
+def random():
 	mousex = randint(0,40)*10
 	mousey = randint(0,40)*10
-	pygame.draw.rect( DISPLAY, RED, [mousex, mousey, 10, 10], 0 )
+	R = [mousex, mousey]
+	return R
 
+def movement(direction):
+	if (direction == 2):
+		A = [ snake[0][0]-10, snake[0][1] ]
+		snake.insert(0, A)
+		snake.pop()
+		draw_snake()
+	if (direction == 3):
+		A = [ snake[0][0]+10, snake[0][1] ]
+		snake.insert(0, A)
+		snake.pop()
+		draw_snake()
+	if (direction == 1):
+		A = [ snake[0][0], snake[0][1]-10 ]
+		snake.insert(0, A)
+		snake.pop()
+		draw_snake()
+	if (direction == -1):
+		A = [ snake[0][0], snake[0][1]+10 ]
+		snake.insert(0, A)
+		snake.pop()
+		draw_snake()
 
+def Draw():
+	for X in range(200):
+		pygame.draw.rect(DISPLAY, GREEN, [x, y, 10, 10], 0)
+		clock.tick(3)
+		pygame.display.update()
+		x += 1
+		y += 1
+
+B = random()
+mousecoordinates = B
+flag = 1
 while True:
+	clock.tick(FPS)
+	DISPLAY.fill((0,0,0))
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
-		else:
-			mouse()
-			draw_snake(snake)
+		if event.type == KEYDOWN:
+			if event.key == K_UP:
+				direction = 1
+			if event.key == K_DOWN:
+				direction = -1
+			if event.key == K_RIGHT:
+				direction = 3
+			if event.key == K_LEFT:
+				direction = 2
+		
+	if (direction == 1):
+		movement(direction)
+	if (direction == -1):
+		movement(direction)
+	if (direction == 3):
+		movement(direction)
+	if (direction == 2):
+		movement(direction)
 
-			pygame.display.update()
-			pygame.display.flip()
-			pygame.time.delay(2000)
-			pygame.quit()
-			sys.exit()
+	if (snake[0] == B):
+		snake.insert(0, B)
+		B = random()
+		draw_mouse(B)
+	draw_snake()
+	if (flag == 1):
+		draw_mouse(B)
+	pygame.display.update()
+	
+
+
