e4d22233405c420d4abb8bcc4ee5f8344dd11f8a
Amijeet Thakur <41593252+amijeet@users.noreply.github.com>
2020-05-10 00:08:58 +0530

    fixed some bugs and added a colorful apple

--- snake.py
+++ snake.py
@@ -1,100 +1,122 @@
-import pygame
-import sys
+import pygame, sys, time, random
 from random import seed 
 from random import randint
 
 from pygame.locals import*
 pygame.init()
-GREEN = (0,255,0)
-WHITE = (255,255,255)
-RED = (255,0,0)
-DISPLAY = pygame.display.set_mode((400,400))
+
+W = 400
+H = 400
+DISPLAY = pygame.display.set_mode((W,H))
+pygame.display.set_caption('Dedicated to the snakes of KGP')
 clock = pygame.time.Clock()
 FPS = 10
-snake = [[200,200], [210,200], [220,200]]
-x = 100
-y = 100
+snake = [[20,20], [20,21], [20,22]]
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+LIME = (0, 255, 255)
+BLUE = (0, 255, 255)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+GREEN = (0, 128, 0)
+MAROON = (128, 0, 0)
+PINK = (255, 20, 147)
+colours = [YELLOW, RED, LIME, BLUE, CYAN, MAGENTA, GREEN, MAROON, PINK]
+flag = 1
 direction = 0
 
-def draw_snake():
-	for s in snake:
-		pygame.draw.rect( DISPLAY, GREEN, [s[0], s[1], 10, 10], 0 )
-def draw_mouse(mouse_co):
-	pygame.draw.rect( DISPLAY, RED, [mouse_co[0], mouse_co[1], 10, 10], 0 )
+def random_apple():
+	count = 0
+	A = [[0 for i in range(2)] for j in range(1444)]
+	for i in range(1, 39):
+		for j in range(1, 39):
+			A[count][0] = i
+			A[count][1] = j
+			count += 1
+	print(len(A))
+	for x in snake:
+		A.remove(x)
+		print(x)
+	return [ A[randint(0,len(A)-1)][0] , A[randint(0,len(A)-1)][1] ]
+	#pygame.draw.rect(DISPLAY, RED, (A[randint(0,len(A)-1)][0]*10, A[randint(0,len(A)-1)][1]*10, 10, 10))
 
-def random():
-	mousex = randint(0,40)*10
-	mousey = randint(0,40)*10
-	R = [mousex, mousey]
-	return R
+def display_snake():
+	for s in snake:
+		pygame.draw.rect(DISPLAY, YELLOW, (s[0]*10, s[1]*10, 10, 10))
 
 def movement(direction):
-	if (direction == 2):
-		A = [ snake[0][0]-10, snake[0][1] ]
-		snake.insert(0, A)
+	if (direction == 2): #left
+		B = [ snake[0][0]-1, snake[0][1] ]
+		snake.insert(0, B)
 		snake.pop()
-		draw_snake()
-	if (direction == 3):
-		A = [ snake[0][0]+10, snake[0][1] ]
-		snake.insert(0, A)
+		display_snake()
+	if (direction == 3): #right
+		B = [ snake[0][0]+1, snake[0][1] ]
+		snake.insert(0, B)
 		snake.pop()
-		draw_snake()
-	if (direction == 1):
-		A = [ snake[0][0], snake[0][1]-10 ]
-		snake.insert(0, A)
+		display_snake()
+	if (direction == 1): #up
+		B = [ snake[0][0], snake[0][1]-1 ]
+		snake.insert(0, B)
 		snake.pop()
-		draw_snake()
-	if (direction == -1):
-		A = [ snake[0][0], snake[0][1]+10 ]
-		snake.insert(0, A)
+		display_snake()
+	if (direction == -1): #down
+		B = [ snake[0][0], snake[0][1]+1 ]
+		snake.insert(0, B)
 		snake.pop()
-		draw_snake()
+		display_snake()
 
-def Draw():
-	for X in range(200):
-		pygame.draw.rect(DISPLAY, GREEN, [x, y, 10, 10], 0)
-		clock.tick(3)
-		pygame.display.update()
-		x += 1
-		y += 1
+def draw_apple(r_apple):
+	colour_number = randint(0, len(colours)-1)
+	pygame.draw.rect(DISPLAY, colours[colour_number], (r_apple[0]*10, r_apple[1]*10, 10, 10))
+	pygame.display.update()
 
-B = random()
-mousecoordinates = B
-flag = 1
-while True:
+r_apple = random_apple()
+print("initial coordinates of apple")
+print(r_apple)
+running = True
+while(running):
 	clock.tick(FPS)
-	DISPLAY.fill((0,0,0))
+	DISPLAY.fill((0, 0, 0))
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
 		if event.type == KEYDOWN:
-			if event.key == K_UP:
+			if ( (event.key == K_UP and direction != -1) or (direction == 1 and event.key == K_DOWN) ):
 				direction = 1
-			if event.key == K_DOWN:
+			if ( (event.key == K_DOWN and direction != 1) or (direction == -1 and event.key == K_UP) ):
 				direction = -1
-			if event.key == K_RIGHT:
+			if ( (event.key == K_RIGHT and direction != 2) or (direction == 3 and event.key == K_LEFT) ):
 				direction = 3
-			if event.key == K_LEFT:
+			if ( (event.key == K_LEFT and direction != 3) or (direction == 2 and event.key == K_RIGHT) ):
 				direction = 2
+		if event.type == KEYDOWN:
+			if event.key == K_SPACE:
+				flag = 1
 		
-	if (direction == 1):
+	if (direction == 1): #up
 		movement(direction)
-	if (direction == -1):
+	if (direction == -1): #down
 		movement(direction)
-	if (direction == 3):
+	if (direction == 3): #right
 		movement(direction)
-	if (direction == 2):
+	if (direction == 2): #left
 		movement(direction)
 
-	if (snake[0] == B):
-		snake.insert(0, B)
-		B = random()
-		draw_mouse(B)
-	draw_snake()
-	if (flag == 1):
-		draw_mouse(B)
-	pygame.display.update()
+	if (snake[0] == r_apple):
+		if (direction == 1):
+			snake.insert(0, [r_apple[0], r_apple[1]-1])
+		if (direction == -1):
+			snake.insert(0, [r_apple[0], r_apple[1]+1])
+		if (direction == 3):
+			snake.insert(0, [r_apple[0]+1, r_apple[1]])
+		if (direction == 2):
+			snake.insert(0, [r_apple[0]-1, r_apple[1]])
+		print(snake)
+		r_apple = random_apple()
+		draw_apple(r_apple)
+
+	display_snake()
+	draw_apple(r_apple)
 	
-
-
